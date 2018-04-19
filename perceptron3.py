2457a5af41a52b273457ab313e8b9efc9852e56f
delouzada <delouzada@gmail.com>
2018-04-19 09:49:46 -0300

    Add files via upload
    É interessante ver como a rede neural se adapta aos dados e consegue achar uma resposta.
    Além desse exemplo estou implementando uma rede neural integrando com o Watson.

---
+++ perceptron3.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 17:55:00 2017
+
+@author: delou
+"""
+
+import numpy as np
+#Operador AND
+#entradas = np.array([[0,0],[0,1],[1,0],[1, 1  ]])
+#saidas = np.array([0,0,0,1])
+
+#Operador OR
+entradas = np.array([[0,0],[0,1],[1,0],[1, 1  ]])
+saidas = np.array([0,1,1,1])
+
+pesos = np.array([0.0, 0.0])
+taxaAprendizagem = 0.1
+
+def stepfunction(soma):
+    
+    if(soma >= 1):
+        return 1
+    return 0
+
+def calculaSaida(registro):
+    s = registro.dot(pesos)
+    return stepfunction(s)
+
+def treinar():
+    
+    erroTotal = 1
+    while(erroTotal != 0):
+        erroTotal = 0
+        for i in range(len(saidas)):
+            saidaCalculada = calculaSaida(np.asarray(entradas[i]))
+            erro = abs(saidas[i] - saidaCalculada)
+            erroTotal += erro
+            for j in  range(len(pesos)):
+                pesos[j] = pesos[j] + (taxaAprendizagem * entradas[i][j] * erro)
+                print('Peso atualizado: ' + str(pesos[j]))
+        print('Total de Erros: ' + str(erroTotal))            
+        
+treinar()
+print('Rede neural treinada')
+
+print(calculaSaida(entradas[0]))
+print(calculaSaida(entradas[1]))
+print(calculaSaida(entradas[2]))
+print(calculaSaida(entradas[3]))
